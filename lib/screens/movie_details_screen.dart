e1dbb514243d6e506f4dd18d5aac26ba16e26767
Gisele <gisesonia@gmail.com>
2021-02-01 20:54:10 -0300

    Textos da página de detalhes dos filmes

--- lib/screens/movie_details_screen.dart
+++ lib/screens/movie_details_screen.dart
@@ -6,9 +6,75 @@ class MovieDetailsScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final routeArgs =
+        ModalRoute.of(context).settings.arguments as Map<String, String>;
+    final id = routeArgs['id'];
+    final rating = routeArgs['rating'];
+
     return Container(
       child: Scaffold(
-          appBar: AppBar(), backgroundColor: Colors.black, body: Text('teste')),
+        appBar: AppBar(),
+        backgroundColor: Colors.black,
+        body: SingleChildScrollView(
+          padding: const EdgeInsets.symmetric(
+            vertical: 10,
+            horizontal: 20,
+          ),
+          child: Column(
+            children: <Widget>[
+              Center(
+                child: Card(
+                  elevation: 5,
+                  child: Hero(
+                    tag: id,
+                    child: Container(
+                      height: 450,
+                      width: 300,
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(5),
+                        image: DecorationImage(
+                          fit: BoxFit.cover,
+                          image: AssetImage(
+                            routeArgs['imageUrl'],
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                routeArgs['title'],
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 28,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 2.5,
+                ),
+                textAlign: TextAlign.center,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                routeArgs['description'],
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                  height: 1.5,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
