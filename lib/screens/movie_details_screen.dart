816b1cbb10cb444ba61e86fd10f9f53a189ac282
Gisele <gisesonia@gmail.com>
2021-02-01 20:36:00 -0300

    Tela de detalhe

---
+++ lib/screens/movie_details_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class MovieDetailsScreen extends StatelessWidget {
+  static const routeName = '/movie-details';
+  const MovieDetailsScreen({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text('teste'),
+    );
+  }
+}
